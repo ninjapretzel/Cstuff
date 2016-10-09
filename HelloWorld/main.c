cf470e66e5c766e04450f5dc3efe4e17c74c391b
ninjapretzel <ninjapretzel@yahoo.com>
2016-10-09 16:12:31 -0600

    Removes exe and o files, that shouldn't have ever been committed anyway.

--- HelloWorld/main.c
+++ HelloWorld/main.c
@@ -2,5 +2,7 @@
 #include <stdlib.h>
 int main() {
     printf("Hello world!\n");
+    printf("I don't know what I'm doing!\n");
+    printf("%f\n%d\n%f\n0x%x\n", .31, 12, 13.37, 0xDEADBEEF);
     return 0;
 }
